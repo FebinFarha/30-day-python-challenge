442f2701d41e164edc4b3009d0644ec24141ed21
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-08 20:03:08 +0530

    Add files via upload

---
+++ Febin/day1.py
@@ -0,0 +1,40 @@
+print('python 30 day challenge')
+print()
+print('day 1')
+print()
+print('1.1 PRINTING A STRING')
+print()
+str="Despite the forecast,live like it's spring."
+print(str)
+print()
+print('1.2 VOLUME OF A SPHERE')
+print()
+r=6
+vol=(4*3.14*r*r*r)/3
+print('Volume of the sphere with radius 6:',vol)
+print()
+print('1.3 ADDITION AND MULTIPLICATION OF TWO NUMBERS')
+print()
+n1=int(input('Enter the number:',))
+n2=int(input('Enter the number:',))
+sum_=n1+n2
+mul=n1*n2
+print('sum of',n1,'and',n2,'is:',sum_)
+print('product of',n1,'and',n2,'is:',mul)
+print()
+print('1.4 BODY MASS INDEX')
+print()
+height=float(input('Enter the height:'))
+weight=float(input('Enter the weight:'))
+bmi=weight/(height*height)
+print('Body Mass Index:',bmi)
+print()
+print('1.5 AVERAGE MARKS')
+print()
+sub1=float(input('Enter the mark:'))
+sub2=float(input('Enter the mark:'))
+sub3=float(input('Enter the mark:'))
+avg=(sub1+sub2+sub3)/3
+print('Average Marks:',avg)
+
+
