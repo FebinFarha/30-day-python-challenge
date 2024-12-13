37da007a3a998facf8a8589cb47d2450cc5856ac
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-13 21:31:52 +0530

    Update Aleena

---
+++ Aleena/day2.py
@@ -0,0 +1,38 @@
+Day 2
+Program 1
+import math
+x=int(input("Enter a value:"))
+print(math.sin(x))
+print(math.cos(x))
+print(math.tan(x))
+
+
+Program 2
+import math
+r=int(input("Enter the radius of cylinder:"))
+h=int(input("Enter the height of cylinder:"))
+area=2*math.pi*r*(r+h)
+print("Area of the cylinder is",area)
+
+
+Program 3
+str="Hi my name is Aleena"
+print('Hi'in str)
+print('Hello'in str)
+
+
+Program 4
+str1="Hello"
+str2="World"
+str3="World"
+print(str1 is str2)
+print(str2 is str3)
+
+
+Program 5 
+a=int(input("Enter the 1st number:"))
+b=int(input("Enter the 2nd number:"))
+quotient=a//b
+remainder=a%b
+print("Quotient is",quotient)
+print("Remainder is",remainder)
