550287f4947648690d9a7ece2b2f799ffe8e0b7c
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-13 21:27:13 +0530

    Update Maria

---
+++ Maria/day4.py
@@ -0,0 +1,36 @@
+Day 4
+Program 1
+i=2
+while i<=10:
+    print(i)
+    i+=2
+
+Program 2
+i=1
+while i<=10:
+    print(i)
+    i+=2
+
+Program 3
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+Program 4
+i=0
+while i<10:
+    print(i)
+    i+=1
+
+Program 5
+i=10
+while i<=300:
+    print(i)
+    i+=10
+
+Program 6
+n=10
+while n<=300:
+    print(n)
+    n+=10
