ec817d71f8fdafdc03760bde123b01dfcd808f80
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-13 21:31:04 +0530

    Update Aleena

---
+++ Aleena/day1.py
@@ -0,0 +1,36 @@
+Day 1
+Program 1
+str="Despite the forecast,live like it's spring"
+print(str)
+
+
+Program 2
+r=6
+v=(4*3.14*r*r*r)/3
+print("Volume of sphere is",v)
+
+Program 3
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+sum=a+b
+product=a*b
+print("Sum is:",sum)
+print("Product is:",product)
+
+
+Program 4
+w=float(input("Enter your Weight in kg:"))
+h=float(input("Enter your Height in m:"))
+bmi=w/(h*h)
+print("BMI is:",bmi,"kg/m^2")
+
+
+Program 5
+sub1=int(input("Enter subject 1 mark:"))
+sub2=int(input("Enter subject 2 mark:"))
+sub3=int(input("Enter subject 3 mark:"))
+avg=(sub1+sub2+sub3)/3
+print("Subject 1:",sub1)
+print("Subject 2:",sub2)
+print("Subject 3:",sub3)
+print("Average mark:",avg)
