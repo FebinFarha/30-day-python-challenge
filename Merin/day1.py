b2529d58008923f304eb6dc85dd9fd007a7cc901
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-13 21:34:18 +0530

    Update Merin

---
+++ Merin/day1.py
@@ -0,0 +1,28 @@
+print("deapite the focast,live like its spring")
+
+r=int(input("enter the radius"))
+radius=4/3*22/7*r**3
+print("radius",radius)
+
+a=int(input("enter the first value"))
+b=int(input("enter the second value"))
+sum=a+b
+product=a*b
+print("sum",sum)
+print("product",product)
+
+mrk1=int(input("enter the mark"))
+mrk2=int(input("enter the mark"))
+mrk3=int(input("enter the mark"))
+sum=mrk1+mrk2+mrk3
+avg=sum/3
+print("avg",avg)
+
+
+
+
+w=int(input("enter your weight"))
+h=int(input("enter your height"))
+bmi=w/h**2
+print(bmi)
+
