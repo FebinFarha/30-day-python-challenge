46a58c43fc5de2aec7280ee84f2c2e6a47e31d9f
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-13 21:25:55 +0530

    Update Maria

---
+++ Maria/day1.py
@@ -0,0 +1,32 @@
+python
+Day 1
+Program 1
+string=("Despite the forecast,live like its spring")
+print(string)
+
+Program 2
+r=6
+v=4/3*3.14*r*r*r
+print("Volume of the spring=",v)
+
+Program 3
+n1=int(input("Enter the number"))
+n2=int(input("Enter the number"))
+s=n1+n2
+m=n1*n2
+print("Sum=",s)
+print("Product=",m)
+
+Program 4
+h=float(input("Enter your height"))
+w=int(input("Enter your weight"))
+bmi=w/(h*h)
+print("BMI=",bmi)
+
+Program 5
+n1=int(input("Enter the mark of subject 1:"))
+n2=int(input("Enter the mark of subject 2:"))
+n3=int(input("Enter the mark of subject 3:"))
+s=n1+n2+n3
+avg=s/3
+print("Average Mark=",avg)
