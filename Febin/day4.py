c1ac934648eb632094e5ca741a943cb0c581cfb4
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-14 19:15:01 +0530

    Update day4.py

--- Febin/day4.py
+++ Febin/day4.py
@@ -1,1 +1,51 @@
-fg
+print('python 30 day challenge')
+print()
+print('day 4')
+print()
+print('4.1 TO PRINT FIRST 10 EVEN NUMBERS USING WHILE LOOP')
+print()
+n=0
+i=1
+while n<20:
+    n=n+2
+    i=i+1
+    print(n)
+print()    
+print('4.2 TO PRINT FIRST 10 ODD NUMBERS USING WHILE LOOP')
+print()
+n=1
+print(n)
+i=1
+while n<20:
+    n=n+2
+    i=i+1
+    print(n)
+print()
+print('4.3 TO PRINT FIRST 10 NATURAL NUMBERS USING WHILE LOOP')
+print()
+n=0
+i=1
+while n<10:
+    n=n+1
+    i=i+1
+    print(n)
+print()
+print('4.4 TO PRINT FIRST 10 WHOLE NUMBERS USING WHILE LOOP')
+print()
+n=0
+print(n)
+i=1
+while n<10:
+    n=n+1
+    i=i+1
+    print(n)
+print()
+print('4.6 TO PRINT FOLLOWING SERIES 10,20,30....300 USING WHILE LOOP')
+print()
+n=10
+print(n,end=' ')
+i=1
+while n<300:
+    n=n+10
+    i=i+1
+    print(n,end=' ')
