100cff46e94355f0de35f53c95722adf41d987fc
FebinFarha <febinfarha2028@cs.sjcetpalai.ac.in>
2024-12-14 11:24:23 +0530

    Update Treesa

---
+++ Treesa/day1.py
@@ -0,0 +1,24 @@
+print("Despite the forecast,live like its spring")
+
+import math
+v=math.pi*6**3
+print ("volume of sphere:",v)
+
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=a+b
+d=a*b
+print("sum:",c)
+print("product:",d)
+
+w=float(input("enter the weight of the body"))
+h=float(input("enter the height of the body"))
+m=float(input("enter the mass of the body"))
+b=w/(h**2*(m**2))
+print("body mass index:",b)
+
+a=float(input("enter the physics mark:"))
+b=float(input("enter the maths mark:'))
+c=float(input("enter the chemistry mark:"))
+s=a+b+c/3
+print ("average:",s)
