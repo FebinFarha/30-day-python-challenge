154d7ba3584b9eefcfb32e86a2b4816d4a288468
Gayathri-B-Nair <gayathribnair2028@cs.sjcetpalai.ac.in>
2024-12-15 13:07:50 +0530

    Update day4.py

--- Gayathri/day4.py
+++ Gayathri/day4.py
@@ -1,1 +1,50 @@
-day3
+print('python 30 day challenge')
+print()
+print('day 4')
+print()
+print('4.1 TO PRINT FIRST 10 EVEN NUMBERS USING WHILE LOOP')
+print()
+i=0
+while i<20:
+    i=i+2
+    print(i)
+print()    
+print('4.2 TO PRINT FIRST 10 ODD NUMBERS USING WHILE LOOP')
+print()
+i=1
+print(i)
+while i<20:
+    i=i+2
+    print(i)
+print()
+print('4.3 TO PRINT FIRST 10 NATURAL NUMBERS USING WHILE LOOP')
+print()
+
+i=0
+while i<10:
+    i=i+1
+    print(i)
+print()
+print('4.4 TO PRINT FIRST 10 WHOLE NUMBERS USING WHILE LOOP')
+print()
+i=0
+print(i)
+while i<10:
+    i=i+1
+    print(i)
+print()
+print('4.5 TO PRINT FIRST 10 NATURAL NUMBERS IN REVERSE ORDER USING WHILE LOOP ')
+i=10
+print(i)
+while i>1:
+    i=i-1
+    print(i)
+print('4.6 TO PRINT FOLLOWING SERIES 10,20,30....300 USING WHILE LOOP')
+print()
+n=10
+print(n,end=' ')
+i=1
+while n<300:
+    n=n+10
+    i=i+1
+    print(n,end=' ')
